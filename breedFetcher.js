3a01c655a99fd3297337f1ce51853494f07d3eeb
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-16 22:05:27 -0500

    updated breedFetcher.js so that it exports fetchBreedDescription and new file index.js handles it and the console logs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,25 @@
 const request = require("request");
 
-const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
-const breed = process.argv[2];
-const url = `${apiUrl}?q=${breed}`;
+const fetchBreedDescription = function(breed, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
+  request(apiUrl, (error, response, body) => {
 
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) { //found out this is typically used to check for errors because http responds with 200 when successfully connected
-    console.error('Status code: ', response.statusCode);
-    return;
-  }
-
-  if (body) {
-    const data = JSON.parse(body);
-
-    if (data.length > 0) {
-      console.log(data[0].description);
-    } else {
-      console.log('Breed not found. Please try again.');
+    if (error) {
+      callback(error, null);
+      return;
     }
-  }
-});
+
+    if (body) {
+      const data = JSON.parse(body);
+
+      if (data.length > 0) {
+        callback(null, data[0].description);
+      } else {
+        callback('Breed not found. Please try again.');
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
